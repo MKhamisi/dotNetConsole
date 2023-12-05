9f22a9dce3f4b6e044419c56c60e75f3dfb0434f
Mariam Khamisi <mariam5100@yahoo.com>
2023-12-05 12:36:15 +0300

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to figure this coding thing out!I probably should ptactice daily");
+        }
+    }
+}
